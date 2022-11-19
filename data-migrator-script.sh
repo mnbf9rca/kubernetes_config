244bb7a5faed8a4aaac9e30717c059131a43550c
rob al <mnbf9rca@users.noreply.github.com>
2022-11-19 18:08:59 +0000

    tweak command

--- data-migrator-script.sh
+++ data-migrator-script.sh
@@ -2,7 +2,7 @@
 # check if DONT_RUN is set
 if [ -z "$DONT_RUN" ]; then
     echo "DONT_RUN is not set - running the script"
-    rsync -vcrog --timeout=20 /source/* /target/
+    rsync -va --no-o --no-g --timeout=20 /source/* /target/
 else
     echo "DONT_RUN is set"
 fi
