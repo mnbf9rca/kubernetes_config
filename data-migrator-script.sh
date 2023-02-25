07d73520262edcfa97afdc41e5e328df65907243
rob al <mnbf9rca@users.noreply.github.com>
2023-02-25 00:06:13 +0000

    update fs to pve2

--- data-migrator-script.sh
+++ data-migrator-script.sh
@@ -4,5 +4,5 @@ if [ -z "$DONT_RUN" ]; then
     echo "DONT_RUN is not set - running the script"
     rsync -va --no-o --no-g --timeout=20 /source/* /target/
 else
-    echo "DONT_RUN is set"
+    echo "DONT_RUN is set - skipping script"
 fi
